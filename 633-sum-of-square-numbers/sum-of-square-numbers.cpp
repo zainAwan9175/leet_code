70106ae2f4f8bbafc5769187f76e2dc7ea00be57
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2024-06-17 15:41:52 +0500

    Time: 3 ms (68.00%) | Memory: 6.9 MB (99.60%) - LeetSync

---
+++ 633-sum-of-square-numbers/sum-of-square-numbers.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    bool judgeSquareSum(int c) {
+        if(c<0)
+        {
+            return 0;
+        }
+        int left=0;
+       long long right = static_cast<long long>(sqrt(c));
+       
+       
+        while (left <= right) {
+        long sum = left * left + right * right;
+        if (sum == c) return true;
+        else if (sum > c) right--;
+        else left++;
+        }
+        return false;
+        
+    }
+};
