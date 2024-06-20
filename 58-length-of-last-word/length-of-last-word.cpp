369b399aeab6f2b2ce4a6654af854eea55d8e7b3
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2024-06-21 00:03:29 +0500

    Time: 4 ms (45.61%) | Memory: 7.7 MB (18.01%) - LeetSync

---
+++ 58-length-of-last-word/length-of-last-word.cpp
@@ -0,0 +1,29 @@
+class Solution {
+public:
+    int lengthOfLastWord(string s) {
+        int count=0;
+        int n=s.size()-1;
+      
+        while(s[n]==' ')
+       {
+        n--;
+       }
+        for(int i=n;i>=0;i--)
+        {
+            if(s[i]!=' ')
+            {
+                 count++;
+
+            }else
+            if(s[i]==' ')
+            {
+                return count;
+            }
+           
+
+
+
+        }
+       return count;
+    }
+};
