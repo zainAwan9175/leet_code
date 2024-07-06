1dcd219dcf27ba3491ffda3c52642276866f67a3
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2024-07-06 23:08:03 +0500

    Time: 127 ms (8.12%) | Memory: 36.5 MB (73.96%) - LeetSync

--- 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
+++ 599-minimum-index-sum-of-two-lists/minimum-index-sum-of-two-lists.cpp
@@ -1,30 +1,33 @@
 class Solution {
 public:
     vector<string> findRestaurant(vector<string>& list1, vector<string>& list2) {
-        vector<string> ans;
-        unordered_map<string, int> mp;
-        int n1 = list1.size(), n2 = list2.size();
-        for(int i=0; i<n1; i++){
-            string temp = list1[i];
-            for(int j=0; j<n2; j++){
-                if(list2[j] == temp){
-                    mp[list2[j]] = i+j;
+        map<string,int> mp;
+        for(int i=0;i<list1.size();i++)
+        {
+            for(int j=0;j<list2.size();j++)
+            {
+                if(list1[i]==list2[j])
+                {
+                    mp[list2[j]]=(i+j);
                 }
             }
         }
-
-        int mini = INT_MAX;
-        for(auto it : mp){
-            if(it.second <= mini){
-                mini = it.second;
+        vector<string> vec;
+int min =INT_MAX;
+        for(auto it:mp)
+        {
+            if(it.second<=min)
+            {
+                min=it.second;
             }
         }
-        for(auto it : mp){
-            if(it.second == mini){
-                ans.push_back(it.first);
+        for(auto it:mp)
+        {
+            if(it.second==min)
+            {
+                vec.push_back(it.first);
             }
         }
-
-        return ans;
+        return vec;
     }
 };
