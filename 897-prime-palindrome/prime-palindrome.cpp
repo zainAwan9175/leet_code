b94cb16226db65a7a4aab5414e783482e11fc68d
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2024-07-10 08:46:33 +0500

    Time: 516 ms (13.68%) | Memory: 7.3 MB (64.79%) - LeetSync

---
+++ 897-prime-palindrome/prime-palindrome.cpp
@@ -0,0 +1,56 @@
+class Solution {
+public:
+
+
+
+
+  bool isprime(int n){
+        if(n%2 == 0) return false;
+        for(int i = 3; i <= pow(n, 0.5); i++){
+            if(n%i == 0) return false;
+        }
+        return true;
+    }
+
+
+    bool ispalindrome(int x) {
+        if (x < 0) {
+            return false;
+        }
+        unsigned int temp = x;
+        unsigned int reverse = 0;
+        while (temp > 0) {
+            int last = temp % 10;
+            reverse = reverse * 10 + last;
+            temp = temp / 10;
+        }
+        return reverse == x;
+    }
+
+    
+    int primePalindrome(int n) {
+    
+        if(n==1||n==2)
+        {
+            return 2;
+        }
+        if (n%2==0)
+        {
+         n++;
+        }
+        while (true) {
+            bool prime = isprime(n);
+            bool palindrome = ispalindrome(n);
+
+            if (prime && palindrome) {
+                return n;
+            }
+            n=n+2;
+          if(n > 1e7 && n < 1e8) n = 1e8 + 1;
+            if(n > 1e5 && n < 1e6) n = 1e6 + 1;
+            if(n > 1e3 && n < 1e4) n = 1e4 + 1;
+            if(n > 11 && n < 100) n = 101;
+           
+        }
+    }
+};
