41c28864a7f5d89b484bcc3ac0b401883f2ed8fc
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2024-07-10 09:23:02 +0500

    Time: 0 ms (100.00%) | Memory: 12.3 MB (8.01%) - LeetSync

---
+++ 35-search-insert-position/search-insert-position.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int searchInsert(vector<int>& nums, int target) {
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]==target)
+            {
+                return i;
+            }else if(nums[i]>target)
+            {
+                return i;
+            }else if(i==nums.size()-1)
+            {
+                return i+1;
+            }
+        }
+        return 0;
+    }
+};
