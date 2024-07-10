1a6854b281a9af14ffd0ab2ceb9609af616bbf34
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2024-07-10 08:56:45 +0500

    Time: 517 ms (13.68%) | Memory: 7 MB (90.34%) - LeetSync

--- 897-prime-palindrome/prime-palindrome.cpp
+++ 897-prime-palindrome/prime-palindrome.cpp
@@ -5,7 +5,14 @@ public:
 
 
   bool isprime(int n){
-        // if(n%2 == 0) return false;
+     if(n==1)
+     {
+        return false;
+     } 
+     else if(n==2)
+     {
+        return true;
+     }
         for(int i = 2; i <= pow(n, 0.5); i++){
             if(n%i == 0) return false;
         }
