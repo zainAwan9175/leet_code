15107456317e546a82c269d8b971640ebcdcbd65
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2024-07-22 22:41:37 +0500

    Time: 7 ms (62.98%) | Memory: 34.9 MB (6.39%) - LeetSync

---
+++ 3397-find-the-integer-added-to-array-i/find-the-integer-added-to-array-i.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    int addedInteger(vector<int>& nums1, vector<int>& nums2) {
+        sort(nums1.begin(),nums1.end());
+        sort(nums2.begin(),nums2.end());
+        
+        
+          int  ans=nums2[0]-nums1[0];
+        return ans;
+
+
+        
+    }
+};
