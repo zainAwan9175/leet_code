7ba24158adaa6c3f0b8d315bd3d07d1c5f0d88c4
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2024-08-16 09:33:03 +0500

    Time: 7 ms (85.04%) | Memory: 16.4 MB (6.12%) - LeetSync

---
+++ 1-two-sum/two-sum.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+    map<int,int> mp;
+    for(int i=0;i<nums.size();i++)
+    {
+        mp[nums[i]]=i;
+    }
+   
+    for(int i=0;i<nums.size();i++)
+ 
+    {
+            int sum=target-nums[i];
+        if(mp.count(sum))
+        {
+            if(i!=mp[sum])
+            {
+                return {i,mp[sum]};
+            }
+        }
+    }
+    return {};
+    }
+};
