b925bfc571457b96c49c3181f100d677c5d7ed0d
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2024-09-05 07:32:55 +0500

    Time: 34 ms (36.86%) | Memory: 27.4 MB (10.11%) - LeetSync

--- 835-linked-list-components/linked-list-components.cpp
+++ 835-linked-list-components/linked-list-components.cpp
@@ -13,27 +13,20 @@ public:
     int numComponents(ListNode* head, vector<int>& nums) {
         // Step 1: Store the elements of nums in an unordered_map for O(1) lookups
         unordered_map<int, bool> numMap;
+        ListNode* temp = head;
         for (int num : nums) {
             numMap[num] = true;
         }
 
         int componentCount = 0;
-        bool inComponent = false;
-        
-        // Step 2: Traverse the linked list
-        while (head != nullptr) {
-            // Step 3: If the current value is in the numMap
-            if (numMap.find(head->val) != numMap.end()) {
-                // If we are not already in a component, we start a new one
-                if (!inComponent) {
-                    componentCount++;
-                    inComponent = true;
+        while (temp != NULL) {
+            if (numMap[temp->val]) {
+                componentCount++;
+                while (temp != NULL && temp->next != NULL && numMap[temp->next->val]) {
+                    temp = temp->next;
                 }
-            } else {
-                // If the current value is not in the map, we are not in a component
-                inComponent = false;
             }
-            head = head->next;
+            temp = temp->next;
         }
 
         return componentCount;
