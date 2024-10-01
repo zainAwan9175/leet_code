436437156c54e2f3fd64062499a9b9c71e15c409
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2024-10-02 00:14:22 +0500

    Time: 0 ms (100.00%) | Memory: 8.4 MB (78.07%) - LeetSync

--- 225-implement-stack-using-queues/implement-stack-using-queues.cpp
+++ 225-implement-stack-using-queues/implement-stack-using-queues.cpp
@@ -1,35 +1,37 @@
 class MyStack {
 public:
-    queue<int> q1, q2;
-
     MyStack() {
+
+  
         
     }
+            queue<int> q1,q2;
     
     void push(int x) {
+        while(!q1.empty())
+        {
+            q2.push(q1.front());
+            q1.pop();
+
+        }
         q1.push(x);
+
+        while(!q2.empty())
+         {
+             q1.push(q2.front());
+             q2.pop();
+               
+        }
     }
     
     int pop() {
-        int x = q1.back();
-        while (!q1.empty()) {
-            if (q1.front() == q1.back()) {
-                q1.pop();
-            }
-            else {
-                q2.push(q1.front());
-                q1.pop();
-            }
-        }
-        while (!q2.empty()) {
-            q1.push(q2.front());
-            q2.pop();
-        }
-        return x;
+        int n=q1.front();
+        q1.pop();
+        return n;
     }
     
     int top() {
-        return q1.back();
+        return q1.front();
     }
     
     bool empty() {
