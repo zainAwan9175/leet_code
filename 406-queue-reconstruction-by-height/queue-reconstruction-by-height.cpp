c8bf546ec087de70d2a7df2fe79019a316a508d7
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2024-10-22 11:04:33 +0500

    Time: 21 ms (87.06%) | Memory: 15.8 MB (37.19%) - LeetSync

--- 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
+++ 406-queue-reconstruction-by-height/queue-reconstruction-by-height.cpp
@@ -1,20 +1,21 @@
 class Solution {
 public:
-static bool comp(vector<int>&a,vector<int>&b)
+static  bool comp(vector<int>&a,vector<int> &b)
 {
-    if(a[0]==b[0])
-    {
+      if(a[0]==b[0])
+      {
         return a[1]<b[1];
-    }
-    return a[0]>b[0];
+      }
+      return b[0]<a[0];
 }
+
     vector<vector<int>> reconstructQueue(vector<vector<int>>& people) {
-        sort(people.begin(),people.end(),comp);
-         vector<vector<int>> ans;
-         for(auto it:people)
-         {
-            ans.insert(ans.begin()+it[1],it);
-         }
-        return ans;
+       sort(people.begin(),people.end(),comp);
+       vector<vector<int>>ans;
+       for(auto it:people)
+       {
+        ans.insert(ans.begin()+it[1],it);
+       }
+       return ans;
     }
 };
