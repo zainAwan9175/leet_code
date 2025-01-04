d496c24a8772f3f7044f8fcbfd638f5006628059
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-01-04 22:36:11 +0500

    Time: 3 ms (5.32%) | Memory: 12.5 MB (53.22%) - LeetSync

--- 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
+++ 1603-running-sum-of-1d-array/running-sum-of-1d-array.cpp
@@ -1,14 +1,31 @@
 class Solution {
 public:
+
     vector<int> runningSum(vector<int>& nums) {
-        int sum=0;
-        vector<int> arr;
-        for(int i=0;i<nums.size();i++)
-        {
-            sum =sum +nums[i];
-            arr.push_back(sum);
-        }
-        return arr; 
-        
+
+vector<int> ans(nums.size());
+for(int i=0;i<nums.size();i++)
+{
+    int sum=0;
+    for(int j=0;j<=i;j++)
+    {
+        sum=sum+nums[j];
+    }
+
+    ans[i]=sum;
+}
+
+return ans;
+
+
     }
 };
+
+
+
+
+
+
+
+
+
