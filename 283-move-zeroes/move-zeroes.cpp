3888243d0f44ca483f9c44dd5ea2b339ef380477
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-01-09 20:33:51 +0500

    Time: 0 ms (100.00%) | Memory: 23.8 MB (37.98%) - LeetSync

---
+++ 283-move-zeroes/move-zeroes.cpp
@@ -0,0 +1,21 @@
+class Solution {
+public:
+    void moveZeroes(vector<int>& nums) {
+        int nonZeroPos=0;
+
+        for(int i=0;i<nums.size();i++)
+        {
+            if(nums[i]!=0)
+            {
+                nums[nonZeroPos]=nums[i];
+                nonZeroPos++;
+            }
+        }
+
+        for(int i=nonZeroPos;i<nums.size();i++)
+        {
+            nums[i]=0;
+        }
+
+    }
+};
