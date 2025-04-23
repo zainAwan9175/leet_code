901eef2c071d9f7191e84260cd1a4a870e035da5
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-04-23 17:03:15 +0500

    Time: 184 ms (5.50%) | Memory: 17.9 MB (71.98%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -1,12 +1,58 @@
+// class Solution {
+// public:
+//     vector<int> topKFrequent(vector<int>& nums, int k) {
+
+// // vector<int> vec = {3, 0, 1, 0};
+// //         if(nums==vec)
+// //         {
+// //             return {0};
+// //         }
+//         map<int,int> mp;
+//         for(int i=0;i<nums.size();i++)
+//         {
+//             mp[nums[i]]++;
+//         }
+
+
+//         vector<int>res(k);
+
+//         for(int i=0;i<k;i++)
+//         {
+//             int max=INT_MIN;
+//             for(auto it:mp)
+//             {
+//                 if(it.second>max)
+//                 {
+//                     if(it.first==0)
+//                     {
+                          
+//                     max=it.second;
+//                     }
+//                   if(find(res.begin(),res.end(),it.first)==res.end())
+//                   {
+                    
+//                       res[i]=it.first;
+//                     max=it.second;
+//                   }
+              
+//                 }
+//             }
+//         }
+//         return res;
+//     }
+// };
+
+
+
 class Solution {
 public:
     vector<int> topKFrequent(vector<int>& nums, int k) {
 
-vector<int> vec = {3, 0, 1, 0};
-        if(nums==vec)
-        {
-            return {0};
-        }
+// vector<int> vec = {3, 0, 1, 0};
+//         if(nums==vec)
+//         {
+//             return {0};
+//         }
         map<int,int> mp;
         for(int i=0;i<nums.size();i++)
         {
@@ -14,23 +60,28 @@ vector<int> vec = {3, 0, 1, 0};
         }
 
 
-        vector<int>res(k);
+        vector<int>res;
 
         for(int i=0;i<k;i++)
         {
             int max=INT_MIN;
+            int val=0;
             for(auto it:mp)
             {
                 if(it.second>max)
                 {
+                   
                   if(find(res.begin(),res.end(),it.first)==res.end())
                   {
-                      res[i]=it.first;
+                    
+                 
                     max=it.second;
+                    val=it.first;
                   }
               
                 }
             }
+            res.push_back(val);
         }
         return res;
     }
