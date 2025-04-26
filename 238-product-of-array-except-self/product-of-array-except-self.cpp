a0321f7d3cdf0df70e41dda2c563ba84843ef796
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-04-26 20:09:25 +0500

    Time: 3 ms (38.02%) | Memory: 42.4 MB (8.93%) - LeetSync

--- 238-product-of-array-except-self/product-of-array-except-self.cpp
+++ 238-product-of-array-except-self/product-of-array-except-self.cpp
@@ -1,75 +1,23 @@
-// class Solution {
-// public:
-//     vector<int> productExceptSelf(vector<int>& nums) {
-
-
-//         vector<int> prefix(nums.size(),1);
-//         vector<int> postfix(nums.size(),1);
-//         prefix[0]=1;
-//         for(int i=1;i<nums.size();i++)
-//         {
-//             prefix[i]=prefix[i-1]*nums[i-1];
-//         }
-
-//         postfix[nums.size()-1]=1;
-//         for(int i=nums.size()-2;i>=0;i--)
-//         {
-//             postfix[i]=postfix[i+1]*nums[i+1];
-//         }
-
-// for(int i=0;i<nums.size();i++)
-// {
-//   nums[i]=postfix[i]*prefix[i];
-// }
-// return nums;
-
-
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
 
-int product=1;
-int count_zero=0;
+vector<int> prefix(nums.size(),1);
+vector<int> postfix(nums.size(),1);
+for(int i=1;i<nums.size();i++)
+{
+    prefix[i]=prefix[i-1]*nums[i-1];
+}
+for(int i=nums.size()-2;i>=0;i--)
+{
+    postfix[i]=postfix[i+1]*nums[i+1];
+}
+vector<int> ans(nums.size(),1);
 for(int i=0;i<nums.size();i++)
 {
-     if(nums[i]!=0)
-        {
-                 product=product*nums[i];
-        }
-        else{
-            count_zero++;
-        }
-
+    ans[i]=postfix[i]*prefix[i];
 }
-       vector<int> res(nums.size(),0);
-if(count_zero>1)
-{
-return res;
-}
-       
-
-       for(int i=0;i<nums.size();i++)
-       {
-        if(count_zero==1)
-        {
-             if(nums[i]!=0)
-             {
-              res[i]=0;
-             }
-             else if(nums[i]==0)
-             {
-                res[i]=product;
-             }
-            
-        }
-        else{
-            res[i]=(product/nums[i]);
-        }
-       
-        
-       }
-       
-     
-        return res;
+return ans;
+    
     }
-};
+    };
