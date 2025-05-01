950143cdefc746962bedca0dc5e9ae5bfeb402b3
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-05-01 19:55:51 +0500

    Time: 51 ms (54.90%) | Memory: 29.2 MB (52.27%) - LeetSync

--- 15-3sum/3sum.cpp
+++ 15-3sum/3sum.cpp
@@ -2,47 +2,45 @@ class Solution {
 public:
     vector<vector<int>> threeSum(vector<int>& nums) {
           vector<vector<int>> ans;
-          sort(nums.begin(),nums.end());
-          
-
+           sort(nums.begin(), nums.end());
           for(int i=0;i<nums.size();i++)
           {
-            
-            if (i > 0 && nums[i] == nums[i - 1]) {
-                continue;
-            }
+           if(i>0&&nums[i]==nums[i-1])continue;
+           int l=i+1;
+           int r=nums.size()-1;
+           while(l<r)
+           {
+            int sum=nums[i]+nums[l]+nums[r];
+            if(sum==0)
+            {
+                ans.push_back({nums[i],nums[l],nums[r]});
+                 while(l<r&&nums[l]==nums[l+1])
 
-            int left=i+1;
-            int right=nums.size()-1;
-               while(left<right)
-               {
-                int sum=nums[i]+nums[left]+nums[right];
-                
-                if(sum==0)
+       
+                { 
+                     l++;
+                } 
+                   while(l<r&&nums[r]==nums[r-1])
+               { 
+                     r--;
+                }  
+                l++;
+                r--;
+            }
+            
+
+                else{
+                    if(sum>0)
                 {
-                    ans.push_back({nums[i],nums[left],nums[right]});
-                    while(left<right&&nums[left]==nums[left+1])
-                    {
-                        left++;
-                    }
-                     while(left<right&&nums[right]==nums[right-1])
-                    {
-                        right--;
-                    }
-                    left++;
-                    right--;
-                }
-                else if(sum<0)
-                {
-                    left++;
-                }else{
-                    right--;
-                }
-                
-               }
-              
+                    r--;
+                }   
+                else{
+                    l++;
+                }   
+                }    
+           }
           }
-           return ans;
+          return ans;
     }
       
     };
