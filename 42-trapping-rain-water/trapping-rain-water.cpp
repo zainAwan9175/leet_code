97fa9a02e495f890edeeca67c6b5339e33999c87
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-05-03 23:19:29 +0500

    Time: 0 ms (100.00%) | Memory: 25.8 MB (93.60%) - LeetSync

--- 42-trapping-rain-water/trapping-rain-water.cpp
+++ 42-trapping-rain-water/trapping-rain-water.cpp
@@ -1,50 +1,49 @@
 class Solution {
 
 public:
-   vector<int> find_lefti(vector<int>& height)
-   {
-    int n=height.size();
-        vector<int> lefti(n);
 
-        lefti[0]=height[0];
-        for(int i=1;i<height.size();i++)
-        {
-            lefti[i]=max(lefti[i-1],height[i]);
-        }
-        return lefti;
-   }
-      vector<int> find_righti(vector<int>& height)
-   {
-    int n=height.size();
-        vector<int> lefti(n);
-
-        lefti[n-1]=height[n-1];
-        for(int i=n-2;i>=0;i--)
-        {
-            lefti[i]=max(lefti[i+1],height[i]);
-        }
-        return lefti;
-   }
+    
 
     int trap(vector<int>& height) {
 
 
-
-        vector<int> lefti=find_lefti(height);
-        vector<int> righti=find_righti(height);
-
+int left_h=0;
+int right_h=0;
 
 
-        int sum=0;
-        int w=1;
-        for(int i=0;i<height.size();i++)
+
+int l=0;
+int r=height.size()-1;
+int ans=0;
+while(l<r)
+{
+    if(height[l]<=height[r])
+    {
+        if(left_h<height[l])
         {
-            int h=min(lefti[i],righti[i])-height[i];
-            sum=sum+h;
-
-
+            left_h=height[l];
+            
         }
-return sum;
+        else{
+            ans=ans+(left_h-height[l]);
+        }
+        l++;
+
+    }else{
+        if(right_h<height[r])
+        {
+            right_h=height[r];
+            
+        }
+        else{
+            ans=ans+(right_h-height[r]);
+        }
+        r--;
+
+    }
+}
+return ans;
+
         
     }
 };
