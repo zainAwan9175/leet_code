711a40ca4e86e13a7e6e52adc057f789d48b72ca
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-05-04 15:52:39 +0500

    Time: 0 ms (100.00%) | Memory: 10 MB (58.06%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,36 +1,36 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-        int r=s.size()-1;
-        int l=0;
-     while(l<r)
-     {
-       
-          if(s[l]>=65&&s[l]<=90)
-          {
-              s[l]=s[l]+32;
-          }
-          if(s[r]>=65&&s[r]<=90)
-          {
-              s[r]=s[r]+32;
-          }
-           if(!(s[l]>=97&&s[l]<=122)&&!(s[l]>=48&&s[l]<=57))
-           {
-                 l++;
-                 continue;
-           }
-              if(!(s[r]>=97&&s[r]<=122)&&!(s[r]>=48&&s[r]<=57))
-           {
-                 r--;
-                 continue;
-           }
-           if(s[l]!=s[r])
-           {
+      for(int i=0;i<s.size();i++)
+      {
+        if(s[i]>=65&&s[i]<=90)
+        {
+            s[i]=s[i]+32;
+        }
+      }
+      int l=0;
+      int r=s.size()-1;
+
+      while(l<r)
+      {
+         if(!(s[l]<=122&&s[l]>=97)&&!(s[l]>=48&&s[l]<=57))
+         {
+           l++;
+           continue;
+         }
+          if(!(s[r]<=122&&s[r]>=97)&&!(s[r]>=48&&s[r]<=57))
+         {
+           r--;
+           continue;
+         }
+         if(s[l]!=s[r])
+         {
             return false;
-           }
-          l++;
-          r--;
-     }
+         }
+         l++;
+         r--;
+      }
+        
       return true;
     }
 };
