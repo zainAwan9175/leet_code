9a0ebaed7f13b05356225f1ba765e0d799912e0a
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-05-30 17:10:46 +0500

    Time: 10 ms (57.78%) | Memory: 11.5 MB (44.43%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -1,39 +1,49 @@
 class Solution {
 public:
     string minWindow(string s, string t) {
-        int count = t.size();
-        unordered_map<char, int> mp;
-        
-        for (char c : t) {
-            mp[c]++;
-        }
+       int count=t.size();
+  
+unordered_map<char, int> mp;
+string str="";
+       int l=0;
+       int r=0;
+       int min_l=INT_MAX;
+       int start=1;
+       for(auto it :t)
+       {
+        mp[it]++;
+       }
+       while(l<=r&&r<s.size())
+       {
+          if(mp[s[r]]>0)
+          {
+            count--;
+          }
+             mp[s[r]]--;
+            
+               
 
-        int l = 0, r = 0;
-        int min_l = INT_MAX;
-        int start = 0;
-
-        while (r < s.size()) {
-            if (mp[s[r]] > 0) {
-                count--;
-            }
-            mp[s[r]]--;
-
-            while (count == 0) {
-                if ((r - l + 1) < min_l) {
-                    min_l = r - l + 1;
-                    start = l; // ⬅️ Store the starting index
+                while(l<=r&& count==0)
+                { 
+                     if((r-l+1)<min_l)
+                {
+                    min_l=r-l+1;
+                  start=l;
                 }
+                    mp[s[l]]++;
+                    if(mp[s[l]]>0)
+                    {
+                        count++;
+                        
+                    }
+                    l++;
 
-                mp[s[l]]++;
-                if (mp[s[l]] > 0) {
-                    count++;
                 }
-                l++;
-            }
+            
+    
+        r++;
 
-            r++;
-        }
-
-        return min_l == INT_MAX ? "" : s.substr(start, min_l);
+       }
+  return min_l == INT_MAX ? "" : s.substr(start, min_l);
     }
 };
