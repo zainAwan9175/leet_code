ef45da5336a9d56267772ca4c007571262f5f563
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-05-30 16:50:01 +0500

    Time: 3 ms (83.37%) | Memory: 11.4 MB (83.36%) - LeetSync

--- 76-minimum-window-substring/minimum-window-substring.cpp
+++ 76-minimum-window-substring/minimum-window-substring.cpp
@@ -3,37 +3,37 @@ public:
     string minWindow(string s, string t) {
         if (s.size() < t.size()) return "";
 
-        unordered_map<char, int> tMap;
-        for (char c : t) tMap[c]++;  
+        unordered_map<char, int> mp;
+        for (char c : t) {
+            mp[c]++;
+        }
 
-        int left = 0, minLen = INT_MAX, start = 0;
-        int required = tMap.size();  
-        int formed = 0;             
+        int count = t.size(); // Number of characters we need to match
+        int left = 0, right = 0;
+        int minLen = INT_MAX;
+        int start = 0;
 
-        unordered_map<char, int> windowCounts;
-
-        for (int right = 0; right < s.size(); right++) {
-            char c = s[right];
-            windowCounts[c]++;
-
-            if (tMap.count(c) && windowCounts[c] == tMap[c]) {
-                formed++;
+        while (right < s.size()) {
+            // If s[right] is needed
+            if (mp[s[right]] > 0) {
+                count--;
             }
 
-            
-            while (left <= right && formed == required) {
-                if (right - left + 1 < minLen) {
-                    minLen = right - left + 1;
+            mp[s[right]]--; // Always decrement
+            right++;
+
+            // When all characters are matched
+            while (count == 0) {
+                if (right - left < minLen) {
+                    minLen = right - left;
                     start = left;
                 }
 
-               
-                char l = s[left];
-                windowCounts[l]--;
-                if (tMap.count(l) && windowCounts[l] < tMap[l]) {
-                    formed--;
+                // Shrink from left
+                mp[s[left]]++;
+                if (mp[s[left]] > 0) {
+                    count++; // We need this character again
                 }
-
                 left++;
             }
         }
