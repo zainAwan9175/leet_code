bd1499954ac99b5ceaa7f766d16f591e449b229c
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-06-17 18:51:54 +0500

    Time: 0 ms (100.00%) | Memory: 13.5 MB (19.02%) - LeetSync

--- 206-reverse-linked-list/reverse-linked-list.cpp
+++ 206-reverse-linked-list/reverse-linked-list.cpp
@@ -8,23 +8,42 @@
  *     ListNode(int x, ListNode *next) : val(x), next(next) {}
  * };
  */
+// class Solution {
+// public:
+//     ListNode* reverseList(ListNode* head) {
+//         stack<int>st;
+//         ListNode* temp=head;
+//         while(temp!=NULL)
+//         {
+//             st.push(temp->val);
+//                     temp=temp->next;
+//         }
+//         temp=head;
+//          while(temp!=NULL)
+//         {
+//         temp->val=st.top();
+//         st.pop();
+//         temp=temp->next;
+//         }
+// return head;
+//     }
+// };
+
 class Solution {
 public:
     ListNode* reverseList(ListNode* head) {
-        stack<int>st;
-        ListNode* temp=head;
-        while(temp!=NULL)
-        {
-            st.push(temp->val);
-                    temp=temp->next;
-        }
-        temp=head;
-         while(temp!=NULL)
-        {
-        temp->val=st.top();
-        st.pop();
-        temp=temp->next;
-        }
-return head;
+     ListNode* cur=head;
+     ListNode* pre=NULL;
+     ListNode* next=NULL;
+     while(cur!=NULL)
+     {
+        next=cur->next;
+        cur->next=pre;
+        pre=cur;
+        cur=next;
+
+
+     }
+     return pre;
     }
 };
