f5d7693820f4cc2a5959773db03e1d219af54dc2
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-06-23 18:14:32 +0500

    Time: 8 ms (81.49%) | Memory: 11.7 MB (96.52%) - LeetSync

--- 141-linked-list-cycle/linked-list-cycle.cpp
+++ 141-linked-list-cycle/linked-list-cycle.cpp
@@ -6,53 +6,26 @@
  *     ListNode(int x) : val(x), next(NULL) {}
  * };
  */
-// class Solution {
-// public:
-//     bool hasCycle(ListNode *head) {
-//         vector<ListNode *> visited;
-//         ListNode *temp=head;
-
-
-//         while(temp!=NULL)
-//         {
-//             for(auto it:visited)
-//             {
-//                 if(it==temp)
-//                 {
-//                     return true;
-//                 }
-//             }
-//             visited.push_back(temp);
-//             temp=temp->next;
-
-//         }
-
-//         return false;
-//     }
-// };
-
-
 class Solution {
 public:
     bool hasCycle(ListNode *head) {
-      if(head==NULL||head->next==NULL)
-      {
-          return false;
-      }
-       ListNode *fast=head->next;
-       ListNode *slow=head;
-
-
-       while(slow!=fast)
-       {
-        if(fast->next==NULL||fast->next->next==NULL)
+  
+      
+if(head==NULL||head->next==NULL)
+{
+return false;
+}
+ListNode *fast=head->next;
+ListNode *slow=head;
+        while(slow!=fast)
         {
-            return false;
+            if(fast->next==NULL||fast->next->next==NULL)
+            {
+                return false;
+            }
+            slow=slow->next;
+            fast=fast->next->next;
         }
-        slow=slow->next;
-        fast=fast->next->next;
-       }
-
-        return true;;
+        return true;
     }
 };
