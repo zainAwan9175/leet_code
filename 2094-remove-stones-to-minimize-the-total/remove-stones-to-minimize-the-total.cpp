f067be81616b249e5560d111d563bb03971dbe1f
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-07-11 16:24:35 +0500

    Time: 291 ms (56.70%) | Memory: 108.8 MB (29.40%) - LeetSync

--- 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.cpp
+++ 2094-remove-stones-to-minimize-the-total/remove-stones-to-minimize-the-total.cpp
@@ -2,25 +2,26 @@ class Solution {
 public:
     int minStoneSum(vector<int>& piles, int k) {
         priority_queue<int> q;
-
-
-        int sum=0;
-        for(auto it :piles)
+        for(auto it:piles)
         {
             q.push(it);
-            sum=sum+it;
         }
 
         for(int i=0;i<k;i++)
         {
             int n=q.top();
             q.pop();
-            sum=sum-n;
-           int res= ceil(n / 2.0);
-           sum=sum+res;
-           q.push(res);
-
+            n=ceil(n/2.0);
+            if(n>0)
+            q.push(n);
         }
-        return sum;
+
+        int ans=0;
+        while(!q.empty())
+        {
+            ans=ans+q.top();
+            q.pop();
+        }
+        return ans;
     }
 };
