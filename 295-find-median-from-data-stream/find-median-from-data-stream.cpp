b907453b1e17ed960c69c07e37c49f366077866f
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-07-11 15:10:09 +0500

    Time: 81 ms (20.30%) | Memory: 124.9 MB (73.76%) - LeetSync

--- 295-find-median-from-data-stream/find-median-from-data-stream.cpp
+++ 295-find-median-from-data-stream/find-median-from-data-stream.cpp
@@ -7,35 +7,24 @@ priority_queue<int,vector<int>,greater<int>> min_q;
     }
     
     void addNum(int num) {
-        if(max_q.empty()||num<max_q.top())
-        {
-                   max_q.push(num);
-        }
-        else{
-            min_q.push(num);
-        }
-
-        if(max_q.size()>min_q.size()+1)
-        {
-            int n=max_q.top();
-            max_q.pop();
-            min_q.push(n);
-        }else if(min_q.size()>max_q.size())
+        max_q.push(num);   
+        min_q.push(max_q.top());
+        max_q.pop();    
+        if(min_q.size()>max_q.size())
         {
             max_q.push(min_q.top());
             min_q.pop();
         }
-        
-        
     }
     
     double findMedian() {
-        if(min_q.size()==max_q.size())
-        {
-            return(min_q.top()+max_q.top())/2.0;
-        }else{
-            return max_q.top();
-        }
+       if(max_q.size()==min_q.size())
+       {
+        return (min_q.top()+max_q.top())/2.0;
+       }else{
+
+        return max_q.top();
+       }
     }
 };
 
