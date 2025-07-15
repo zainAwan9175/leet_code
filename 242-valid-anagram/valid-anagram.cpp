d1faff8c04bdccb60cbb97c4a816f4868a136884
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-07-15 15:28:27 +0500

    Time: 5 ms (29.87%) | Memory: 9.5 MB (90.99%) - LeetSync

--- 242-valid-anagram/valid-anagram.cpp
+++ 242-valid-anagram/valid-anagram.cpp
@@ -1,27 +1,8 @@
 class Solution {
 public:
     bool isAnagram(string s, string t) {
-  
-map<char,int> st;
-  
-map<char,int> ss;
-if(s.size()!=t.size())
-{
-    return false;
-}
-for(int i=0;i<s.size();i++)
-{
-    st[t[i]]++;
-    ss[s[i]]++;
-
-}
-if(st==ss)
-{
-    return true;
-}
-else{
-    return false;
-}
-        
+        sort(s.begin(),s.end());
+        sort(t.begin(),t.end());
+        return s==t;
     }
 };
