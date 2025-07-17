2fdd70d5d68c7685ea7e2aa57c250fb3723ff071
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-07-17 17:02:41 +0500

    Time: 6 ms (40.20%) | Memory: 24.1 MB (50.85%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,30 +1,49 @@
 class Solution {
 public:
-
-
     bool isValidSudoku(vector<vector<char>>& board) {
-    unordered_set<string> st;
-    for(int i=0;i<9;i++)
-    {
-        for(int j=0;j<9;j++)
-        {
-            if(board[i][j]=='.')continue;
-            string value=string(1,board[i][j]);
-            string row=value+"row"+to_string(i);
-            string col=value+"col"+to_string(j);
-            string box=value+"box"+to_string(i/3)+"_"+to_string(j/3);
-
-            if(st.find(row)!=st.end()||st.find(col)!=st.end()||st.find(box)!=st.end())
+        for(int i=0;i<9;i++)
+        { 
+            unordered_set<char> row;
+            unordered_set<char> col;
+            for(int j=0;j<9;j++)
             {
-                return false;
-            }
-            else{
-                st.insert(row);
-                st.insert(col);
-                st.insert(box);
+                
+                if(board[i][j]!='.'&&row.count(board[i][j]))
+                {
+                    return false;
+                }else if(board[i][j]!='.'){
+                    row.insert(board[i][j]);
+                }
+
+                if(board[j][i]!='.'&&col.count(board[j][i]))
+                {
+                    return false;
+                }else if(board[j][i]!='.'){
+                    col.insert(board[j][i]);
+                }
             }
         }
-    }
-    return true;
+
+
+        for(int i=0;i<9;i=i+3)
+        {
+            for(int j=0;j<9;j=j+3)
+            {unordered_set<char> b;
+                 for(int r=i;r<i+3;r++)
+                 {
+                    for(int c=j;c<j+3;c++)
+                    {
+                           if(board[r][c]!='.'&&b.count(board[r][c]))
+                            {
+                             return false;
+                             }else if(board[r][c]!='.'){
+                                  b.insert(board[r][c]);
+                             }
+
+                    }
+                 }
+            }
+        }
+        return true;
     }
 };
