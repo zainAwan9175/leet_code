3d6c10e21c1a7347839117a60e5ae14cef48af6d
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-07-25 22:41:12 +0500

    Time: 1 ms (48.26%) | Memory: 10 MB (58.57%) - LeetSync

--- 125-valid-palindrome/valid-palindrome.cpp
+++ 125-valid-palindrome/valid-palindrome.cpp
@@ -1,36 +1,30 @@
 class Solution {
 public:
     bool isPalindrome(string s) {
-      for(int i=0;i<s.size();i++)
-      {
-        if(s[i]>=65&&s[i]<=90)
+        int l=0;
+        int r=s.size()-1;
+        while(l<r)
         {
-            s[i]=s[i]+32;
-        }
-      }
-      int l=0;
-      int r=s.size()-1;
+            if(!isalnum(s[l]))
+            {
+                l++;
+                continue;
+            }
+            if(!isalnum(s[r]))
+            {
+                r--;
+                continue;
+             }
 
-      while(l<r)
-      {
-         if(!(s[l]<=122&&s[l]>=97)&&!(s[l]>=48&&s[l]<=57))
-         {
-           l++;
-           continue;
-         }
-          if(!(s[r]<=122&&s[r]>=97)&&!(s[r]>=48&&s[r]<=57))
-         {
-           r--;
-           continue;
-         }
-         if(s[l]!=s[r])
-         {
-            return false;
-         }
-         l++;
-         r--;
-      }
-        
-      return true;
+             if(tolower(s[l])!=tolower(s[r]))
+             {
+                return false;
+             }
+             else{
+                l++;
+                r--;
+             }
+        }
+        return true;
     }
 };
