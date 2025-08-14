129f4a7a131d862ac87dc449d8385edc5df37486
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-08-14 17:08:18 +0500

    Time: 7 ms (36.16%) | Memory: 25.2 MB (38.56%) - LeetSync

--- 36-valid-sudoku/valid-sudoku.cpp
+++ 36-valid-sudoku/valid-sudoku.cpp
@@ -1,29 +1,26 @@
 class Solution {
 public:
     bool isValidSudoku(vector<vector<char>>& board) {
-
-
         unordered_set<string>st;
         for(int i=0;i<9;i++)
         {
-           for(int j=0;j<9;j++)
-           {
-            if(board[i][j]=='.')continue;
-   string value = string(1, board[i][j]);
-
-            string row=value+"row"+to_string(i);
-            string col=value+"col"+to_string(j);
-            string box=value+"box"+to_string(i/3)+to_string(j/3);
-            if(st.count(row)||st.count(col)||st.count(box))
+            for(int j=0;j<9;j++)
             {
-                return false;
+               if(board[i][j]=='.')continue;
+
+               string val=string(1,board[i][j]);
+               string row=val+"row"+to_string(i);
+                string col=val+"col"+to_string(j);
+                string box=val+"box"+to_string(i/3)+to_string(j/3);
+                if(st.find(row)!=st.end()||st.find(col)!=st.end()||st.find(box)!=st.end())
+                {
+                    return false;
+                }else{
+                    st.insert(row);
+                    st.insert(col);
+                    st.insert(box);
+                }
             }
-            else{
-                st.insert(row);
-                st.insert(col);
-                st.insert(box);
-            }
-           }
         }
         return true;
     }
