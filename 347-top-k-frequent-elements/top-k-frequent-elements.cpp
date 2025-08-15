0ead7697a3a99af4461fd82428ed17e953cafc70
Zain Ul Abedin <157986920+zainAwan9175@users.noreply.github.com>
2025-08-15 15:42:36 +0500

    Time: 6 ms (20.33%) | Memory: 17.8 MB (80.64%) - LeetSync

--- 347-top-k-frequent-elements/top-k-frequent-elements.cpp
+++ 347-top-k-frequent-elements/top-k-frequent-elements.cpp
@@ -9,22 +9,21 @@ vector<int>res;
         mp[it]++;
       }
 
-      for(int i=0;i<k;i++)
-      {
+    for(int i=0;i<k;i++)
+    {
         int max=INT_MIN;
         int n=0;
         for(auto it:mp)
         {
             if(it.second>max)
             {
-               max=it.second;
-               n=it.first;
+                max=it.second;
+                n=it.first;
             }
-             
         }
         res.push_back(n);
         mp.erase(n);
-      }
-     return res;
+    }
+    return res;
     }
 };
